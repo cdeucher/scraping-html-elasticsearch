9ae1fb6761a3c4533c35227b908c8b603f91988c
cristianoboell <cristianoboell@yahoo.com.br>
2018-04-18 20:09:23 -0300

    FIRST

---
+++ app.js
@@ -0,0 +1,40 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+var request = require('request')
+  , cheerio = require('cheerio');
+
+var dbport = (process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 80);
+var dbhost = (process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function (socket) {
+   socket.emit('news', { hello: 'world' });
+
+   socket.on('toServer', function (data) {
+    console.log(data);
+    var URL = data;//'http://socket.io';
+      request(URL, function(err, res, body) {
+         if (err) {
+           console.log(err);
+         } else {
+           console.log('Scraping Links');
+            var $ = cheerio.load(body);
+            $('a').each(function(i, link) {
+              console.log('%s', $(link).attr('href'));
+              socket.emit('toClient', '<br>'+$(link).attr('href'));
+            });
+         }
+      });
+        console.log('End');
+    });
+});
+
+
+server.listen(dbport,dbhost);
